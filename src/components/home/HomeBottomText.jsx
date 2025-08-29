3352bb2c47712656108e5bf5ba8963b07ce1a0f1
Rubbas yar khan <rubbasyarkhan007@gmail.com>
2025-08-30 01:37:22 +0500

    home page is done

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Globe } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const HomeBottomText = () => {
-  return (
-    <div>HomeBottomText</div>
-  )
-}
+  const [time, setTime] = useState(new Date());
 
-export default HomeBottomText
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formattedTime = `${time.getHours().toString().padStart(2, '0')}:${time.getMinutes().toString().padStart(2, '0')}:${time.getSeconds().toString().padStart(2, '0')}`;
+
+  return (
+    <div className='w-full relative'>
+      <div>
+        <p className='absolute right-0 bottom-32 w-full lg:w-70 lg:text-[1vw] font-[font1] leading-[1.3vw] px-2'>
+          K72 est une agence qui pense chaque action pour nourrir la marque. Demain, dans 5 mois et dans 5 ans. On cherche la friction qui crée l’étincelle pour générer de l’émotion. Pour assurer une relation honnête, on est sans filtre, on dit ce qui doit être dit, on fait ce qui doit être fait.
+        </p>
+      </div>
+
+      <div className='flex flex-col sm:flex-row justify-between items-end gap-6 sm:gap-10 px-2 mb-3'>
+        <div className='flex items-center gap-4'>
+          <Globe className='h-6 w-6' />
+          <span className='font-mono text-xs sm:text-sm md:text-base uppercase'>
+            Montreal_ <span>{formattedTime}</span>
+          </span>
+        </div>
+
+        <div className='flex gap-4 sm:gap-10 font-[font2] '>
+          <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center justify-center rounded-full uppercase'>
+            <Link className='text-xs sm:text-lg md:text-xl lg:text-[5vw] px-6 sm:px-10' to='/projects'>Projects</Link>
+          </div>
+          <div className='border-2 hover:border-[#D3FD50] hover:text-[#D3FD50] flex items-center justify-center rounded-full uppercase'>
+            <Link className='text-xs sm:text-lg md:text-xl lg:text-[5vw] px-10 sm:px-10' to='/agence'>Agence</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeBottomText;
--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
+import homevideo from '../../assets/Herosectionvid.mp4';
 
 const Video = () => {
-    return (
+  return (
+    <div className="w-full h-full"> 
+      <video
+        // src="https://download-video-ak.vimeocdn.com/v3-1/playback/a278bb9c-996c-4760-8ca1-dd0abff308f7/83c745cf?__token__=st=1756498393~exp=1756501993~acl=%2Fv3-1%2Fplayback%2Fa278bb9c-996c-4760-8ca1-dd0abff308f7%2F83c745cf%2A~hmac=2f9901268d1b6e2bee5ed95dc55afb75a3185be4b27197af287fc055a581e7e4&r=dXMtd2VzdDE%3D"
+        src={homevideo}
+        className="w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+        // playsInline
+        // preload="auto"
+      />
+    </div>
+  );
+};
 
-        <>
-            <div className='w-full h-full' >
-                <video src="src\assets\Herosectionvid.mp4" className='w-full h-full object-cover' autoPlay loop muted ></video>
-
-            </div>
-        </>
-    )
-}
-
-export default Video
+export default Video;
